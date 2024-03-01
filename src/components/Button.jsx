e98507ca9e0be6e650c93ad86cb41b95609b6805
hyeone22 <127266003+hyeone22@users.noreply.github.com>
2024-03-01 22:22:43 +0900

    테일윈드 연습

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,30 @@
-import { styled } from 'styled-components'
-import { useState } from 'react';
+export default function Button({ children, ...props }) {
+  return (
+    <button
+      className=" px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
 
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+// import { styled } from 'styled-components'
+// import { useState } from 'react';
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
 
-export default Button;
+//   &:hover {
+//     background-color: #f0920e;
+//   }
+// `;
+
+// export default Button;
